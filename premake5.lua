71437b6636bda270510bfa44b274216d3d5f9f04
EyesOnly89 <eyesonly89@gmail.com>
2022-05-01 23:27:46 +0200

    premake5 updates for better file management, housecleaning.

--- premake5.lua
+++ premake5.lua
@@ -19,11 +19,3 @@ project "ImGui"
 		"imgui_tables.cpp",
 		"imgui_demo.cpp"
 	}
-
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
-
-	filter { "system:windows", "configurations:Release" }
-		buildoptions "/MT"
